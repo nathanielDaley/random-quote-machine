b82c8713409a8ed36124bf15cebcd26028ee69eb
Nathaniel Daley <ndaley64@gmail.com>
2024-11-18 22:19:37 -0600

    add quotebox content

--- src/components/QuoteBox/QuoteBox.jsx
+++ src/components/QuoteBox/QuoteBox.jsx
@@ -1,15 +1,31 @@
 import "./QuoteBox.css";
+import { FaTwitter, FaQuoteLeft, FaTumblr } from "react-icons/fa";
 
 function QuoteBox({ quote }) {
   return (
-    <section className="quote-box" id="quote-box">
-      <div className="quote__content">
-        <h2 className="quote__quote" id="text">
-          {quote.quote}
-        </h2>
-        <h4 className="quote__author" id="author">
-          {quote.author}
-        </h4>
+    <section className="container" id="quote-box">
+      <div className="card">
+        <div className="card-body">
+          <h2 className="card-title text-center" id="text">
+            <FaQuoteLeft size="30" />
+            &nbsp;{quote.quote}
+          </h2>
+          <h4 className="card-text text-end" id="author">
+            - {quote.author}
+          </h4>
+          <button className="btn btn-primary mt-4">
+            <FaTwitter />
+          </button>
+          <button
+            className="btn btn-primary mt-4"
+            style={{ marginLeft: "10px" }}
+          >
+            <FaTumblr />
+          </button>
+          <button className="btn btn-primary mt-4" style={{ float: "right" }}>
+            New Quote
+          </button>
+        </div>
       </div>
     </section>
   );
